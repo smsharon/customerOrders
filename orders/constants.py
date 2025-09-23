1237b16c2b5c7011d46dd44ce42897e76573dc90
Shallon <shallonmokeira@gmail.com>
2025-09-23 15:47:09 +0300

    feat(constants): add constants used throughout the application

---
+++ orders/constants.py
@@ -0,0 +1,25 @@
+"""
+Constants used throughout the application.
+"""
+# Order states
+DRAFT = "DRAFT"
+PLACED = "PLACED"
+FULFILLED = "FULFILLED"
+CANCELLED = "CANCELLED"
+
+# Transaction actions
+TRANSACTION_ACTIONS = {
+    "CREATE_ORDER": "Order created",
+    "SUBMIT_ORDER": "Order submitted",
+    "FULFILL_ORDER": "Order fulfilled",
+    "CANCEL_ORDER": "Order cancelled",
+    "UPDATE_ORDER": "Order updated",
+    "CUSTOMER_REGISTERED": "Customer registered",
+}
+
+# Inventory statuses
+INVENTORY_STATUS = {
+    "AVAILABLE": "AVAILABLE",
+    "FEW_REMAINING": "FEW REMAINING",
+    "OUT_OF_STOCK": "OUT OF STOCK",
+}
