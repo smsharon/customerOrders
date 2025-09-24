41770f5896bb9411e43a6d3259a0806e05be809b
Shallon <shallonmokeira@gmail.com>
2025-09-24 16:05:23 +0300

    feat(views): add viewsets for Customer, Inventory, and Order

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .models import Customer, Inventory, Order
+from .serializers import CustomerSerializer, InventorySerializer, OrderSerializer
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+class InventoryViewSet(viewsets.ModelViewSet):
+    queryset = Inventory.objects.all()
+    serializer_class = InventorySerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Order.objects.filter(customer__user=user)  
