a22b463fcf652c4243a22fdd55ecc75cde2b5c07
Shallon <shallonmokeira@gmail.com>
2025-09-25 10:16:56 +0300

    feat: add orders app URLs and update core routes

---
+++ orders/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CustomerViewSet, InventoryViewSet, OrderViewSet, TransactionViewSet
+
+router = DefaultRouter()
+router.register(r'customers', CustomerViewSet)
+router.register(r'inventory', InventoryViewSet)
+router.register(r'orders', OrderViewSet)
+router.register(r'transactions', TransactionViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
